b1901e7461d287c811f9eafb706aa770114a40e5
wei756 <wei756fg@gmail.com>
2023-04-04 15:37:20 +0900

    [INITIAL] 리액트 앱 생성

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import styles from './MainPage.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
+function MainPage(props) {
+  return <div className="MainPage">
+    메인이에용
+  </div>
+}
+
+export default MainPage;
