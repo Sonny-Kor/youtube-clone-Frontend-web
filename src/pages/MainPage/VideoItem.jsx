ad6a14266988d147a6c1d20f73fabb5961389e07
wei756 <wei756fg@gmail.com>
2023-05-03 11:37:10 +0900

    [UPDATE] formatCountNumber, formatTime 함수 이동
    - 두 힘수는 VideoItem에서만 쓰이는 함수가 아니기 때문에 글로벌 함수로 분리함
    - 글로벌한 함수로 분리하면서 인자명도 일반적인 이름으로 변경함(createdTime->timestamp)
    - JSDoc 추가함. 함수 정의할 때 description, param, return정도는 적어주기

--- src/pages/MainPage/VideoItem.jsx
+++ src/pages/MainPage/VideoItem.jsx
@@ -1,4 +1,4 @@
-
+import { formatCountNumber, formatTime } from '../../common/functions';
 
 import './VideoItem.scss';
 import VideoItemMore from './VideoItemMore';
@@ -6,48 +6,6 @@ import VideoItemMore from './VideoItemMore';
 
 function VideoItem({title, videoId, thumbnail, profileImg, channelTitle, viewCount, createdTime }) {
   
-
-  const now = new Date();
-
-  const formatViewCount = (views) =>{
-    if (views < 1000) {
-      return views;
-    } else if (views < 10000){
-      return parseInt(views / 100) / 10 + ' 천';
-    } else if (views < 100000000){
-      return parseInt(views / 1000) / 10 + ' 만';
-    } else if (views < 1000000000000){
-      return parseInt (views / 10000) / 10 + ' 억';
-    }
-    return views;
-  }
-  const formatTime = (createdTime) => {
-    const diff = now.getTime() - createdTime * 1000;
-    const minute = 60 * 1000;
-    const hour = 60 * minute;
-    const day = 24 * hour;
-    const month = 30 * day;
-    const year = 12 * month;
-
-    if (diff < minute) {
-      return "방금 전";
-    } else if (diff < hour) {
-      const minutes = Math.floor(diff / minute);
-      return `${minutes}분 전`;
-    } else if (diff < day) {
-      const hours = Math.floor(diff / hour);
-      return `${hours}시간 전`;
-    } else if (diff < month) {
-      const days = Math.floor(diff / day);
-      return `${days}일 전`;
-    } else if (diff < year) {
-      const months = Math.floor(diff / month);
-      return `${months}달 전`;
-    } else {
-      const years = Math.floor(diff / year);
-      return `${years}년 전`;
-    }
-  }
   const onClickHandler = e => {
     console.log(e)
     if (e.target.className === 'btnMore') {
@@ -68,7 +26,7 @@ function VideoItem({title, videoId, thumbnail, profileImg, channelTitle, viewCou
             <div className='VideoTitle'>{title}</div>
             <div className='ChannelName'>{channelTitle}</div>
             <div className='DescriptionWrapper'>
-              <div className='ViewCounter'>조회수 {formatViewCount(viewCount)}</div>
+              <div className='ViewCounter'>조회수 {formatCountNumber(viewCount)}</div>
               <div className='dot'>·</div>
               <div className='UploadedTime'>{formatTime(createdTime)}</div>
             </div>
